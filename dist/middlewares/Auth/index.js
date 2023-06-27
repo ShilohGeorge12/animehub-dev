8b4b5eea897c0990622c020fd15b694cb5ae48a8
Shiloh George <shilohgeorge2019@gmail.com>
2023-06-27 13:29:55 +0100

    Edited Auth Middleware

--- dist/middlewares/Auth/index.js
+++ dist/middlewares/Auth/index.js
@@ -1,35 +1,23 @@
-import Jwt from "jsonwebtoken";
-import { config } from "dotenv";
+import Jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
 config();
 export const genCookie = (req, res, next) => {
     const sercret = process.env.SECRET;
     if (sercret) {
         const signedJwt = Jwt.sign({ token: 'jwt token' }, sercret, { expiresIn: 90 });
-        res
-            .header('x-api-key', signedJwt)
-            .header('access-control-expose-headers', 'x-api-key');
+        res.header('x-api-key', signedJwt).header('access-control-expose-headers', 'x-api-key');
         next();
     }
 };
 export const Auth = (req, res, next) => {
-    // try {
     const sercret = `${process.env.SECRET}`;
     const cookies = req.cookies['key'];
-    // console.log( cookies );
+    console.log(cookies);
     if (cookies) {
         Jwt.verify(cookies, sercret);
         next();
     }
     else {
-        res
-            .status(400)
-            .json({ error: 'invalid Token' });
+        res.status(400).json({ error: 'invalid Token' });
     }
-    ;
-    // } catch (error) {
-    //   console.log(error)
-    //   res
-    //   .status(401)
-    //   .json({ error: 'You Are Not Allowed!' });
-    // };
 };
--- src/middlewares/Auth/index.ts
+++ src/middlewares/Auth/index.ts
@@ -1,37 +1,25 @@
-import { Request, Response, NextFunction } from "express";
-import { AuthType, genCookieType } from "../../types/index.js";
-import Jwt from "jsonwebtoken";
-import { config } from "dotenv";
+import { AuthType, genCookieType } from '../../types/index.js';
+import Jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
 config();
 
-export const genCookie: genCookieType = (req, res, next ) => {
-  const sercret = process.env.SECRET;
-  if( sercret ){
-    const signedJwt = Jwt.sign({token: 'jwt token'},sercret, { expiresIn: 90 });
-    res
-    .header('x-api-key', signedJwt)
-    .header('access-control-expose-headers', 'x-api-key');
-    next();
-  }
+export const genCookie: genCookieType = (req, res, next) => {
+	const sercret = process.env.SECRET;
+	if (sercret) {
+		const signedJwt = Jwt.sign({ token: 'jwt token' }, sercret, { expiresIn: 90 });
+		res.header('x-api-key', signedJwt).header('access-control-expose-headers', 'x-api-key');
+		next();
+	}
 };
 
-export const Auth: AuthType = (req, res, next ) => {
-  // try {
-    const sercret = `${process.env.SECRET}`;
-    const cookies: string = req.cookies['key'];
-    // console.log( cookies );
-    if( cookies ){
-      Jwt.verify(cookies, sercret);
-      next();
-    }else{
-      res
-      .status(400)
-      .json({ error: 'invalid Token' });
-    };
-  // } catch (error) {
-  //   console.log(error)
-  //   res
-  //   .status(401)
-  //   .json({ error: 'You Are Not Allowed!' });
-  // };
+export const Auth: AuthType = (req, res, next) => {
+	const sercret = `${process.env.SECRET}`;
+	const cookies: string = req.cookies['key'];
+	console.log(cookies);
+	if (cookies) {
+		Jwt.verify(cookies, sercret);
+		next();
+	} else {
+		res.status(400).json({ error: 'invalid Token' });
+	}
 };
