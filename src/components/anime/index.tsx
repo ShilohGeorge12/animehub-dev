f081e54d04f772267c96327865ae9cb44dfd4ddc
Shiloh George <shilohgeorge2019@gmail.com>
2023-07-21 13:09:10 +0100

    formatted the main content for seo

--- src/components/anime/index.tsx
+++ src/components/anime/index.tsx
@@ -15,7 +15,7 @@ function Anime({ animes }: IAnimeProps) {
 					const profileImage = URL.createObjectURL(new Blob([new Uint8Array(anime.image.data.data)], { type: anime.image.contentType }));
 
 					return (
-						<article
+						<figure
 							key={anime._id}
 							className='group transition duration-1000 ease-in-out w-36 h-52 rounded-lg flex flex-col items-center justify-center gap-2'
 							onClick={() => naviTo(`/anime/${anime._id}`)}>
@@ -26,10 +26,10 @@ function Anime({ animes }: IAnimeProps) {
 								loading='lazy'
 								className={`w-4/5 rounded-lg mx-auto transition duration-700 ease-in-out group-hover:scale-125`}
 							/>
-							<p className='text-white text-sm font-bold text-ellipsis text-center group-hover:hidden group-hover:font-semibold overflow-hidden whitespace-nowrap w-[95%] mx-auto'>
+							<figcaption className='text-white text-sm font-bold text-ellipsis text-center group-hover:hidden group-hover:font-semibold overflow-hidden whitespace-nowrap w-[95%] mx-auto'>
 								{anime.title}
-							</p>
-						</article>
+							</figcaption>
+						</figure>
 					);
 				})}
 		</div>
